b7b8e29255b3613d6b087c669d90168a76d01044
Alexander Bramall <51518257+AlexBramall@users.noreply.github.com>
2021-12-06 23:10:48 -0800

    Bitch ass bitches

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to the Blomde Moms Blog Bitches!</p>
+        <p>Welcome to the Blomde Moms Blog Bitch ass bitches!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
